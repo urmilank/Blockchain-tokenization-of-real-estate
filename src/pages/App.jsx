649ed278cb9ddf5d6bb562859178f5633912e7b3
urmilank <136303968+urmilank@users.noreply.github.com>
2025-01-24 18:17:06 +0530

    Add files via upload

---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+import Homepage from "./pages/Homepage";
+import TempTransaction from "./pages/TempTransaction";
+
+export default function App() {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
+}
---
+++ src/pages/centralState.js
@@ -0,0 +1,7 @@
+import { atom } from "jotai";
+
+export const currentProperty = atom(null);
+
+export const currentAccount = atom(null);
+
+export const propertiesStore = atom([]);
